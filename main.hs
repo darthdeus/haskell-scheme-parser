a6d8802cca7f7c7bac7229f5598594cdfae4c3c1
Jakub Arnold <darthdeus@gmail.com>
2012-09-17 23:17:42 +0200

    Add whitespace skipping parser

--- main.hs
+++ main.hs
@@ -11,9 +11,12 @@ symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
 
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+-- If I understand this correctly the >> just means a sequence
+readExpr input = case parse (spaces >> symbol) "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
 
+spaces :: Parser ()
+spaces = skipMany1 space
 
 
